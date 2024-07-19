396e1d813872d79bf58d5855db17c75ebe8fa99d
MaxSavostyanov <maxsavostyanov34@gmail.com>
2024-07-19 23:08:29 +0300

    Докеризируйте бэкенд

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -1,9 +1,10 @@
 export default () => ({
   port: Number(process.env.SERVER_PORT) || 3000,
-  dbHost: 'localhost',
+  dbHost: process.env.POSTGRES_HOST || 'localhost',
   dbPort: Number(process.env.DB_PORT) || 5432,
   dbName: process.env.DB_NAME || 'kupipodariday',
   dbUsername: process.env.DB_USERNAME || 'student',
   dbPassword: process.env.DB_PASSWORD || 'student',
-  jwtKey: 'secret_jwt_key',
+  jwtKey: process.env.JWT_SECRET || 'secret_jwt_key',
+  jwtExpiers: process.env.JWT_EXPIERS || '7d',
 });
